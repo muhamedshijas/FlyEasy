a06818446e4c7122accd4da7de1af8cfcdcd6baa
muhamedshijas <shijushijas157@gmail.com>
2023-08-26 06:49:09 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <MDBContainer fluid>
+      <h1>Hello welocme</h1>
+    </MDBContainer>
+  );
+}
+
+export default App;
